337c6f4310d7ad79c6488f0c1d499890bc296d59
Stephan Behnke <stephanos@users.noreply.github.com>
2026-06-06 14:57:33 -0700

    Make task queue recorder opt in

--- tests/testcore/onebox.go
+++ tests/testcore/onebox.go
@@ -111,6 +111,7 @@ type (
 		taskQueueRecorder         *TaskQueueRecorder
 		spanExporters             map[telemetry.SpanExporterType]sdktrace.SpanExporter
 		tokenProvider             auth.TokenProvider
+		enableTaskQueueRecorder   bool
 	}
 
 	// FrontendConfig is the config for the frontend service
@@ -169,6 +170,7 @@ type (
 		HostsByProtocolByService map[transferProtocol]map[primitives.ServiceName]static.Hosts
 		SpanExporters            map[telemetry.SpanExporterType]sdktrace.SpanExporter
 		TokenProvider            auth.TokenProvider
+		EnableTaskQueueRecorder  bool
 	}
 
 	listenHostPort string
@@ -212,6 +214,7 @@ func newTemporal(t *testing.T, params *TemporalParams) *TemporalImpl {
 		replicationStreamRecorder:        NewReplicationStreamRecorder(),
 		spanExporters:                    params.SpanExporters,
 		tokenProvider:                    params.TokenProvider,
+		enableTaskQueueRecorder:          params.EnableTaskQueueRecorder,
 	}
 
 	// Configure output file path for on-demand logging (call WriteToLog() to write)
@@ -427,8 +430,10 @@ func (c *TemporalImpl) startHistory() {
 		c.chasmVisibilityMgr = chasmVisibilityManager
 	}).Apply(c.testHooks, testhooks.GlobalScope)
 
-	c.taskQueueRecorder = NewTaskQueueRecorder(c.logger)
-	testhooks.NewHook(testhooks.HistoryTasksWritten, c.taskQueueRecorder.Record).Apply(c.testHooks, testhooks.GlobalScope)
+	if c.enableTaskQueueRecorder {
+		c.taskQueueRecorder = NewTaskQueueRecorder(c.logger)
+		testhooks.NewHook(testhooks.HistoryTasksWritten, c.taskQueueRecorder.Record).Apply(c.testHooks, testhooks.GlobalScope)
+	}
 
 	for _, host := range c.hostsByProtocolByService[grpcProtocol][serviceName].All {
 		var namespaceRegistry namespace.Registry
